2845cbea1529a186ec144e79829bc759aa6ef6cb
Bochkovskyi <unundertale1@ukr.net>
2023-07-27 12:11:27 +0300

    update bullets

--- src/js/bullets.js
+++ src/js/bullets.js
@@ -1,10 +1,18 @@
-const crewBullets = document.querySelectorAll("[data-bullet-target]");
+const crewBullets = document.querySelectorAll("[data-crew-bullet-target]");
 const crewContents = document.querySelectorAll(".crew__info");
 const crewImages = document.querySelectorAll(".crew__img-container");
 
+const technologyBullets = document.querySelectorAll(
+  "[data-tech-bullet-target]"
+);
+const technologyContents = document.querySelectorAll(".technology__info");
+const technologyImages = document.querySelectorAll(
+  ".technology__img-container"
+);
+
 crewBullets.forEach((bullet, index) => {
   bullet.addEventListener("click", () => {
-    const target = document.querySelector(bullet.dataset.bulletTarget);
+    const target = document.querySelector(bullet.dataset.crewBulletTarget);
 
     resetActiveState(crewBullets);
     resetActiveState(crewContents);
@@ -13,5 +21,20 @@ crewBullets.forEach((bullet, index) => {
     setActiveState(bullet);
     setActiveState(target);
     setActiveState(crewImages[index]);
+  });
+});
+
+technologyBullets.forEach((bullet, index) => {
+  bullet.addEventListener("click", () => {
+    const target = document.querySelector(bullet.dataset.techBulletTarget);
+    console.log(target);
+
+    resetActiveState(technologyBullets);
+    resetActiveState(technologyContents);
+    resetActiveState(technologyImages);
+
+    setActiveState(bullet);
+    setActiveState(target);
+    setActiveState(technologyImages[index]);
   });
 });
